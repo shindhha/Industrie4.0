855efaae629445eff23edd16a57fb3fcf104a421
julesrumeau <jules.rumeau1@gmail.com>
2025-10-30 09:39:21 +0100

    third commit

--- tutu.gd
+++ tutu.gd
@@ -21,17 +21,33 @@ func _ready():
 	# Initialisation visuelle
 	_update_state()
 
+
+func post_api(url: String, body: Dictionary) -> void:
+	var http := HTTPRequest.new()
+	add_child(http)
+
+	# Connecter le signal pour récupérer la réponse
+	http.request_completed.connect(func(result, response_code, headers, response_body):
+		print("POST vers", url)
+		print("→ Code:", response_code)
+		print("→ Réponse:", response_body.get_string_from_utf8())
+		http.queue_free()  # Nettoyer après usage
+	)
+
+	# Config POST
+	var headers = ["Content-Type: application/json"]
+	var json_body = JSON.stringify(body)
+
+	var err = http.request(url, headers, HTTPClient.METHOD_POST, json_body)
+	if err != OK:
+		push_error("Erreur d'envoi POST : %s" % err)
 func _on_off_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		# if (is_on):
 			print("off")
 			is_on = false
+			post_api("http://127.0.0.1:5000/api/devices/led-001/write", {"state": "off"})
 			emit_signal("button_toggled", true)
-			var url = "http://127.0.0.1:5000/api/devices/led-001/write"
-			var headers = ["Content-Type: application/json"]
-			var body = JSON.stringify({"state": "off"})
-			var response = await HTTPRequest.new().request_async(url, headers, HTTPClient.METHOD_POST, body)
-			print("Réponse :", response.body.get_string_from_utf8())
 	
 
 func _on_on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
@@ -39,11 +55,8 @@ func _on_on_input_event(camera: Node, event: InputEvent, event_position: Vector3
 			print("on")
 			is_on = false
 			emit_signal("button_toggled", false)
-			var url = "http://127.0.0.1:5000/api/devices/led-001/write"
-			var headers = ["Content-Type: application/json"]
-			var body = JSON.stringify({"state": "on"})
-			var response = await HTTPRequest.new().request_async(url, headers, HTTPClient.METHOD_POST, body)
-			print("Réponse :", response.body.get_string_from_utf8())
+			post_api("http://127.0.0.1:5000/api/devices/led-001/write", {"state": "on"})
+
 
 func _on_button_toggled(new_state: bool) -> void:
 	is_on = new_state
